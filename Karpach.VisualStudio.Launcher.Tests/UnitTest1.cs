b9932750d57941e83c750dc29c21dd9f2c9f474e
Viktar Karpach <karpach@gmail.com>
2024-10-13 13:17:24 -0500

    Add test project

---
+++ Karpach.VisualStudio.Launcher.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Karpach.VisualStudio.Launcher.Tests
+{
+	public class Tests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			Assert.Pass();
+		}
+	}
+}
